02bba0aa111c9dc0983625a756546d5f7c7513de
Mike <mjohanson@gesdate.com>
2018-09-30 23:09:22 -0700

    remove support for azureMobile.js and use .env

--- src/configuration/defaults.js
+++ src/configuration/defaults.js
@@ -2,63 +2,62 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
 var winston = require('winston'),
-    path = require('path'),
-    environment = require('../utilities/environment'),
-    packageJson = require('../../package.json');
+  path = require('path'),
+  environment = require('../utilities/environment'),
+  packageJson = require('../../package.json')
 
 module.exports = function () {
-    return {
-        platform: 'express',
-        basePath: basePath(),
-        configFile: 'azureMobile',
-        promiseConstructor: Promise,
-        apiRootPath: '/api',
-        tableRootPath: '/tables',
-        notificationRootPath: '/push/installations',
-        swaggerPath: '/swagger',
-        authStubRoute: '/.auth/login/:provider',
-        debug: environment.debug,
-        version: 'node-' + packageJson.version,
-        apiVersion: '2.0.0',
-        homePage: false,
-        swagger: false,
-        maxTop: 1000,
-        pageSize: 50,
-        userIdColumn: 'userId',
-        logging: {
-            level: environment.debug ? 'debug' : 'info',
-            transports: [
-                new (winston.transports.Console)({
-                    colorize: true,
-                    timestamp: true,
-                    showLevel: true
-                })
-            ]
-        },
-        cors: {
-            exposeHeaders: 'Link,Etag',
-            maxAge: 300,
-            hostnames: ['localhost']
-        },
-        data: {
-            provider: 'mssql',
-            schema: 'dbo',
-            dynamicSchema: true,
-            server: 'localhost',
-            database: 'master',
-            user: 'sa',
-            password:  'R43$fof983l!'
-        },
-        auth: {
-            secret: '0000'
-        },
-        notifications: { },
-        storage: { }
-    };
+  return {
+    platform: 'express',
+    basePath: basePath(),
+    promiseConstructor: Promise,
+    apiRootPath: '/api',
+    tableRootPath: '/tables',
+    notificationRootPath: '/push/installations',
+    swaggerPath: '/swagger',
+    authStubRoute: '/.auth/login/:provider',
+    debug: environment.debug,
+    version: 'node-' + packageJson.version,
+    apiVersion: '2.0.0',
+    homePage: false,
+    swagger: false,
+    maxTop: 1000,
+    pageSize: 50,
+    userIdColumn: 'userId',
+    logging: {
+      level: environment.debug ? 'debug' : 'info',
+      transports: [
+        new (winston.transports.Console)({
+          colorize: true,
+          timestamp: true,
+          showLevel: true
+        })
+      ]
+    },
+    cors: {
+      exposeHeaders: 'Link,Etag',
+      maxAge: 300,
+      hostnames: ['localhost']
+    },
+    data: {
+      provider: 'mssql',
+      schema: 'dbo',
+      dynamicSchema: true,
+      server: 'localhost',
+      database: 'master',
+      user: 'sa',
+      password: 'R43$fof983l!'
+    },
+    auth: {
+      secret: '0000'
+    },
+    notifications: { },
+    storage: { }
+  }
 };
 
-function basePath() {
-    return environment.hosted
-        ? (process.env.HOME || '') + "\\site\\wwwroot"
-        : path.dirname(require.main.filename);
+function basePath () {
+  return environment.hosted
+    ? (process.env.HOME || '') + '\\site\\wwwroot'
+    : path.dirname(require.main.filename)
 }
--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ The azure-mobile-apps module is the Nodejs implementation of Azure Mobile Apps
 */
 
 var loadConfiguration = require('./configuration'),
-    table = require('./express/tables/table'),
-    logger = require('./logger'),
-    promises = require('./utilities/promises'),
-    query = require('./query'),
+  table = require('./express/tables/table'),
+  logger = require('./logger'),
+  promises = require('./utilities/promises'),
+  query = require('./query'),
 
-    platforms = {
-        express: require('./express'),
-    };
+  platforms = {
+    express: require('./express')
+  }
 
 /**
 Creates an instance of the azure-mobile-apps server object for the platform specified in the configuration.
@@ -26,18 +26,17 @@ Express 4.x is currently the only supported platform.
 @returns {module:azure-mobile-apps/src/express}
 */
 module.exports = function (configuration, environment) {
-    return module.exports.create(loadConfiguration.from()
-        .defaults(configuration)
-        .environment(environment)
-        .settingsJson()
-        .file()
-        .object(configuration)
-        .commandLine()
-        .apply());
+  return module.exports.create(loadConfiguration.from()
+    .defaults(configuration)
+    .environment(environment)
+    .settingsJson()
+    .object(configuration)
+    .commandLine()
+    .apply())
 };
 
 module.exports.create = function (configuration) {
-    return platforms[configuration.platform](configuration);
+  return platforms[configuration.platform](configuration)
 };
 
 /**
@@ -45,16 +44,16 @@ module.exports.create = function (configuration) {
 @description Creates an instance of a table definition object
 @returns {module:azure-mobile-apps/src/express/tables/table}
 */
-module.exports.table = table;
+module.exports.table = table
 
 /** @type {module:azure-mobile-apps/src/logger} */
-module.exports.logger = logger;
+module.exports.logger = logger
 
 /** @type {module:azure-mobile-apps/src/query} */
-module.exports.query = query;
+module.exports.query = query
 
 /** @type {module:azure-mobile-apps/src/utilities/promises} */
-module.exports.promises = promises;
+module.exports.promises = promises
 
 // this is purely a helper function to allow intellisense for custom API definitions
-module.exports.api = function (definition) { return definition; };
+module.exports.api = function (definition) { return definition };
